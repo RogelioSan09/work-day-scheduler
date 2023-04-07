35e3b0677b608ab1dfbea7595a1688fd082ece69
RogelioSan09 <rogeliosnchz@gmail.com>
2023-04-07 00:54:08 -0700

    Initial code for categorizing time block based on point in time

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,25 @@
 $(function () {
+  /*Executes the function to determine the time category of the time blocks
+    Compares the current time with the time designated by each time block.*/
+  timeSequence();
+
+  function timeSequence(){
+    var currentTime = dayjs().format('H');
+    console.log(currentTime);
+    for (var i = 0; i < 8; i++){
+      var timeBlock = 9 + i;
+      console.log(timeBlock);
+      if (currentTime > timeBlock){
+        $('#hour').children().addClass("past");
+      }
+      if (currentTime < timeBlock){
+        $('#hour').children().addClass("future");
+      } else{
+        $('#hour').children().addClass("present");
+      }
+    }
+  }
+
   /*Displays the current date*/
   var today = dayjs();
   $('#currentDay').text(today.format('dddd MMMM DD, YYYY'));
