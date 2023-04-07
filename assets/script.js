2439eeeddc514bccfd08aeacd1760af57e8ab5cb
RogelioSan09 <rogeliosnchz@gmail.com>
2023-04-06 22:24:56 -0700

    Added an event listener

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,22 @@
 $(function () {
+  /*Declared variables for save button and assigned them to the id within the html.*/
+  var saveButton = $('.saveBtn');
+  console.log(saveButton);
+  var userEvents = $('#user-input');
+  console.log(userEvents);
+
+  /*Event listener for saving users events in local storage upon user clicking the save button.*/
+  saveButton.on('click', '.saveBtn', function(){
+    localStorage.setItem("user-input", userEvents);
+  });
+
+  /*Retrieve stored events from local storage*/
+  renderEvents();
+  
+  function renderEvents(){
+    localStorage.getItem("user-input");
+  }
+
   /*Displays the current date*/
   var today = dayjs();
   $('#currentDay').text(today.format('dddd MMMM DD, YYYY'));
