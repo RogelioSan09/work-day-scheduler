d7a76e150910f9fc9c2144be9748a74ce8dc7b6d
RogelioSan09 <rogeliosnchz@gmail.com>
2023-04-07 04:35:47 -0700

    minor adjustments to html, modified event listener(still needs work)

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,53 @@
 $(function () {
-  /*Declared variables for save button and assigned them to the id within the html.*/
+  /*Declared variables for save button and assigned them to the class within the html.*/
   var saveButton = $('.saveBtn');
-  console.log(saveButton);
+  var saveEvents = $('#user-input');
+  console.log(saveEvents);
   
-
   /*Event listener for saving users events in local storage upon user clicking the save button.*/
   saveButton.on('click', function(){
-    var userEvents = $('.description');
-    console.log(userEvents);
-    userEvents.append(userEvents);
-    localStorage.setItem(".description", userEvents);
+    localStorage.setItem('userEvents', saveEvents);
+    console.log(saveEvents);
   });
 
-  /*Retrieve stored events from local storage*/
-  renderEvents();
-  
-  function renderEvents(){
-    var userEvents = localStorage.getItem(userEvents);
+  /*Executes the function to determine the time category of the time blocks
+  Compares the current time with the time designated by each time block.*/
+  timeSequence();
+
+  function timeSequence(){
+      var currentTime = dayjs().format('H');
+      console.log("Current time: " + currentTime);
+      for (var i = 0; i < 9; i++){
+        var timeBlock = 9 + i;
+        console.log("Time-block is " + timeBlock);
+        if (currentTime > timeBlock){
+          console.log(currentTime + ' > ' + timeBlock);
+          for(var j = 0; j < 8; j++){
+              console.log(j);
+              $('textarea').addClass("past");
+          }
+        } if (currentTime < timeBlock){
+          console.log(currentTime + ' < ' + timeBlock);
+          for(var j = 0; j < 8; j++){
+              console.log(j);
+              $('textarea').addClass("future");
+          }
+        } else{
+              console.log(currentTime + ' == ' + timeBlock);
+              for(var j = 0; j < 8; j++){
+                  console.log(j);
+                  $('textarea').addClass("present");
+              }
+        }
+      }
   }
+
+
+  /*Retrieve stored events from local storage*/
+
+  window.addEventListener ('load', function (){
+    $('#user-input') = localStorage.getItem('userEvents');
+  });
 
   /*Displays the current date*/
   var today = dayjs();
