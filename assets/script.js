60afc41df076d469e8349d4116a0b690deefa3fd
RogelioSan09 <rogeliosnchz@gmail.com>
2023-06-15 01:25:13 -0700

    Modified the code within the script.js file. Changed the code that handles adding the class for past, present and future to the time blocks.

--- assets/script.js
+++ assets/script.js
@@ -1,55 +1,44 @@
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that the 
+// code isn't run until the browser has finished rendering all the elementsin the html.
 $(function () {
-  /*Declared variables for save button and assigned them to the class within the html.*/
   var saveButton = $('.saveBtn');
-  var saveEvents = $('#userInput').val($(this).data('textarea'));
+  var saveEvents = $('.description').val($(this).data('textarea'));
   console.log(saveEvents);
-  
-  /*Event listener for saving users events in local storage upon user clicking the save button.*/
-  saveButton.on('click', function(){ 
+
+  saveButton.on('click', function(){
     localStorage.setItem('userEvents', JSON.stringify(saveEvents));
     console.log(saveEvents);
   });
 
-  /*Executes the function to determine the time category of the time blocks
-  Compares the current time with the time designated by each time block.*/
-  timeSequence();
+  var currentHour = dayjs().format('H');
+  var timeBlock = $('.time-block').attr('id');
+  console.log(currentHour);
+  console.log(timeBlock);
 
-  function timeSequence(){
-      var currentTime = dayjs().format('H');
-      console.log("Current time: " + currentTime);
-      for (var i = 0; i < 9; i++){
-        var timeBlock = 9 + i;
-        console.log("Time-block is " + timeBlock);
-        if (currentTime > timeBlock){
-          console.log(currentTime + ' > ' + timeBlock);
-          for(var j = 0; j < 8; j++){
-              console.log(j);
-              $('textarea').addClass("past");
-          }
-        } if (currentTime < timeBlock){
-          console.log(currentTime + ' < ' + timeBlock);
-          for(var j = 0; j < 8; j++){
-              console.log(j);
-              $('textarea').addClass("future");
-          }
-        } else{
-              console.log(currentTime + ' == ' + timeBlock);
-              for(var j = 0; j < 8; j++){
-                  console.log(j);
-                  $('textarea').addClass("present");
-              }
-        }
-      }
+  if (currentHour > timeBlock) {
+    $('.time-block').addClass('past');
+  } else if (currentHour === timeBlock) {
+    $('.time-block').addClass('present');
+  } else {
+    $('.time-block').addClass('future');
   }
 
+  // retrieve the user's events from local storage and display them in the correct time-blocks when the page loads or refreshes
+  var userEvents = JSON.parse(localStorage.getItem('userEvents'));
+  console.log(userEvents);
 
-  /*Retrieve stored events from local storage*/
-
-  window.addEventListener ('load', function (){
-    saveEvents = JSON.parse(localStorage.getItem('userEvents'));
-  });
+  if (userEvents !== null) {
+    $('.description').val(userEvents);
+  } else {
+    $('.description').val('');
+  }
 
   /*Displays the current date*/
   var today = dayjs();
   $('#currentDay').text(today.format('dddd MMMM DD, YYYY'));
+});
+
+/*Retrieve stored events from local storage*/
+window.addEventListener ('load', function (){
+  saveEvents = JSON.parse(localStorage.getItem('userEvents'));
 });
